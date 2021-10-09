a08fa4189756d324a113af678ff34f31544466bc
shubha028 <shubhasharma0208@gmail.com>
2021-10-09 15:15:26 +0530

    changes

--- WieMsit/main/urls.py
+++ WieMsit/main/urls.py
@@ -5,15 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('registerPage', views.registerPage, name='register'),
-    path('volunteer', views.volunteer, name='volunteer'),
-    path('dummy', views.DummyregisterPage, name='dummy'),
-    path('login', views.loginPage, name='login'),
-    path('feedback', views.feedbackview, name='feedback'),
-    path('gallery', views.gallery, name='gallery'),
-    path('volunteerMain', views.volunteerMain, name='youthvolunteer'),
-    path('ngolist', views.listofngos, name='ngolist'),
-    path('ngohome', views.ngohome, name='ngohome'),
+    path('events', views.listofngos, name='events'),
 
 ]
 
